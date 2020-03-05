1e697337f70fed923b09f5acd6f795a1ed0f3736
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:10:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  array.map do |call|
+    "#{call[0].upcase + call[1..-1]}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.find do |food|
+    cheese_types.include?(food)
+  end
 end
