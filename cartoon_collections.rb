9fc0345994cbf8997a024fc139a99f0a36ff8f3d
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:04:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,16 @@ end
 
 def long_planeteer_calls(long_planeteer_calls) # code an argument here
   # Your code here
-  
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
