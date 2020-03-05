f40a09552a4066d7e7fb22fe483410cf72c49f5c
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:03:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,10 @@ end
 
 def summon_captain_planet(veggies) # code an argument here
   # Your code here
-  
+  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    veggies.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
