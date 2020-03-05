b1800c6a314a54dd2672fbad2b1c0367e32977c3
Michael David Sackstein <m_sackstein@yahoo.com>
2020-03-05 18:02:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each_with_index{|d}
+  dwarves.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
